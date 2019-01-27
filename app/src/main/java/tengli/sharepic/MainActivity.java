b259c375a44af3619f7263637f7e0cb20a96fc21
Teng Li <tengli@vt.edu>
2019-01-27 18:43:06 -0500

    add photo caputure function

--- app/src/main/java/tengli/sharepic/MainActivity.java
+++ app/src/main/java/tengli/sharepic/MainActivity.java
@@ -1,22 +1,41 @@
 package tengli.sharepic;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONObject;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int CAMERA_REQUEST = 1888;
+    private ImageView imageView;
+    private static final int MY_CAMERA_PERMISSION_CODE = 100;
+
     private void setText(final TextView text,final String value){
         runOnUiThread(new Runnable() {
             @Override
@@ -25,49 +44,50 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
     public void sendPost(View view) {
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
-                try {
-                    URL url = new URL("https://bkxetb5d8a.execute-api.ap-northeast-1.amazonaws.com/prod/sharepic");
-                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                    conn.setRequestMethod("POST");
-                    conn.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
-                    conn.setRequestProperty("Accept","application/json");
-                    conn.setDoOutput(true);
-                    conn.setDoInput(true);
+            try {
+                URL url = new URL("https://bkxetb5d8a.execute-api.ap-northeast-1.amazonaws.com/prod/sharepic");
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
+                conn.setRequestProperty("Accept","application/json");
+                conn.setDoOutput(true);
+                conn.setDoInput(true);
 
-                    JSONObject jsonParam = new JSONObject();
-                    jsonParam.put("key1", "value1");
-                    jsonParam.put("key2", "value2");
-                    jsonParam.put("key3", "value3");
+                JSONObject jsonParam = new JSONObject();
+                jsonParam.put("key1", "value1");
+                jsonParam.put("key2", "value2");
+                jsonParam.put("key3", "value3");
 
-                    Log.i("Send JSON", jsonParam.toString());
-                    DataOutputStream os = new DataOutputStream(conn.getOutputStream());
-                    os.writeBytes(jsonParam.toString());
+                Log.i("Send JSON", jsonParam.toString());
+                DataOutputStream os = new DataOutputStream(conn.getOutputStream());
+                os.writeBytes(jsonParam.toString());
 
-                    os.flush();
-                    os.close();
+                os.flush();
+                os.close();
 
-                    InputStream in = new BufferedInputStream(conn.getInputStream());
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-                    String line;
-                    StringBuilder result = new StringBuilder();
-                    while ((line = reader.readLine()) != null) {
-                        result.append(line);
-                    }
-
-                    Log.i("STATUS", String.valueOf(conn.getResponseCode()));
-                    Log.i("Json response", result.toString());
-
-                    TextView jsResponse = (TextView) findViewById(R.id.textView);
-                    setText(jsResponse,result.toString());
-
-                    conn.disconnect();
-                } catch (Exception e) {
-                    e.printStackTrace();
+                InputStream in = new BufferedInputStream(conn.getInputStream());
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                String line;
+                StringBuilder result = new StringBuilder();
+                while ((line = reader.readLine()) != null) {
+                    result.append(line);
                 }
+
+                Log.i("STATUS", String.valueOf(conn.getResponseCode()));
+                Log.i("Json response", result.toString());
+
+                TextView jsResponse = (TextView) findViewById(R.id.textView);
+                setText(jsResponse,result.toString());
+
+                conn.disconnect();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
             }
         });
 
@@ -112,5 +132,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.imageView = this.findViewById(R.id.imageView);
+        Button photoButton =  this.findViewById(R.id.button);
+        photoButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                int permissionCheckRead = ContextCompat.checkSelfPermission(getApplicationContext(),
+                        Manifest.permission.READ_EXTERNAL_STORAGE);
+                if (permissionCheckRead
+                        != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions((Activity) getApplicationContext(),
+                            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                            CAMERA_REQUEST);
+                } else {
+                    Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(cameraIntent, CAMERA_REQUEST);
+                }
+            }
+        });
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == MY_CAMERA_PERMISSION_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "camera permission granted", Toast.LENGTH_LONG).show();
+                Intent cameraIntent = new
+                        Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            } else {
+                Toast.makeText(this, "camera permission denied", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(photo);
+        }
+    }
+
+
 }
